58e7e03980d89789655862ab428592ef5c5c04c9
Ryan-009 <2476204430@qq.com>
2021-09-28 17:29:26 +0800

    create a xcodeproj

---
+++ MutualAid/MutualAid/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MutualAid
+//
+//  Created by 吴凯耀 on 2021/9/28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MutualAid/MutualAid/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MutualAid
+//
+//  Created by 吴凯耀 on 2021/9/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
